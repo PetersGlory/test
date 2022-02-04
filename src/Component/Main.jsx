0f0f4e67a4a4a62d970b6e78953fb0f974bf39f3
PetersGlory <peterglo2200@gmail.com>
2022-02-04 17:06:25 +0100

    test commit

---
+++ src/Component/Main.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import { Sidenav } from './Sidenav';
+
+export const Main = () => {
+  return <div>
+      <Sidenav />
+  </div>;
+};
---
+++ src/Component/Sidenav.js
@@ -0,0 +1,65 @@
+import React, { useEffect, useState } from "react"
+import "./SelectionBox.css"
+
+export const Sidenav =()=>{
+    const [color, setColor] = useState(false)
+    // const box = document.getElementsByClassName(".box")
+    const handleDrag =(e)=>{
+        e.preventDefault();
+        setColor(true);
+       if (color ===true) {
+        e.target.style.background = 'blue';
+       }
+    }
+    const handleDrop = () =>{
+        var elements = document.getElementsByClassName("box");
+        for (var i = 0, len = elements.length; i < len; i++) {
+            elements[i].style.background = "white";
+            setColor(false)
+        }
+    }
+    return (
+        <div className="root">
+            <div className="row">
+                <div className="box" onDragOver={handleDrag} onDrop={handleDrop} />
+                <div className="box" onDragOver={handleDrag} onDrop={handleDrop} />
+                <div className="box" onDragOver={handleDrag} onDrop={handleDrop} />
+                <div className="box" onDragOver={handleDrag} onDrop={handleDrop} />
+                <div className="box" onDragOver={handleDrag} onDrop={handleDrop} />
+                <div className="box" onDragOver={handleDrag} onDrop={handleDrop} />
+            </div>
+            <div className="row">
+                <div className="box" onDragOver={handleDrag} onDrop={handleDrop} />
+                <div className="box" onDragOver={handleDrag} onDrop={handleDrop} />
+                <div className="box" onDragOver={handleDrag} onDrop={handleDrop} />
+                <div className="box" onDragOver={handleDrag} onDrop={handleDrop} />
+                <div className="box" onDragOver={handleDrag} onDrop={handleDrop} />
+                <div className="box" onDragOver={handleDrag} onDrop={handleDrop} />
+            </div>
+            <div className="row">
+                <div className="box" onDragOver={handleDrag} onDrop={handleDrop} />
+                <div className="box" onDragOver={handleDrag} onDrop={handleDrop} />
+                <div className="box" onDragOver={handleDrag} onDrop={handleDrop} />
+                <div className="box" onDragOver={handleDrag} onDrop={handleDrop} />
+                <div className="box" onDragOver={handleDrag} onDrop={handleDrop} />
+                <div className="box" onDragOver={handleDrag} onDrop={handleDrop} />
+            </div>
+            <div className="row">
+                <div className="box" onDragOver={handleDrag} onDrop={handleDrop} />
+                <div className="box" onDragOver={handleDrag} onDrop={handleDrop} />
+                <div className="box" onDragOver={handleDrag} onDrop={handleDrop} />
+                <div className="box" onDragOver={handleDrag} onDrop={handleDrop} />
+                <div className="box" onDragOver={handleDrag} onDrop={handleDrop} />
+                <div className="box" onDragOver={handleDrag} onDrop={handleDrop} />
+            </div>
+            <div className="row">
+                <div className="box" onDragOver={handleDrag} onDrop={handleDrop} />
+                <div className="box" onDragOver={handleDrag} onDrop={handleDrop} />
+                <div className="box" onDragOver={handleDrag} onDrop={handleDrop} />
+                <div className="box" onDragOver={handleDrag} onDrop={handleDrop} />
+                <div className="box" onDragOver={handleDrag} onDrop={handleDrop} />
+                <div className="box" onDragOver={handleDrag} onDrop={handleDrop} />
+            </div>
+      </div>
+    )
+}
